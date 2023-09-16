1663701c0065283190553ed478fbdd57ef0fc0fb
Amirsalarsafaei <amirsalar.safaei@divar.ir>
2023-09-16 22:17:07 +0330

    feat: added salar agent

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # author: Vahid Ghafourian
 # Date: 2023/09/06
 import math
+import random
 from typing import List, Any
 
 from Kernel import Kernel
+from clients.salar.SalarAgent import ClientSalar
 from components.game import Game
 from clients.client_ai import ClientAi
 import networkx as nx
@@ -34,15 +36,23 @@ def enable_print():
     sys.stdout = sys.__stdout__
 
 
-def get_clients(kernel, player_turn: int) -> List[Any]:
+def get_clients_specific(kernel, client, player_turn: int) -> List[Any]:
     # Build Enemy clients
     clients = []
     for i in range(3):
         if player_turn == i:
-            clients.append(ClientAi(kernel))
+            clients.append(client(kernel))
         else:
             clients.append(ClientEnemyTwo(kernel))
     return clients
+
+
+def get_clients_random(kernel, *args, **kwargs):
+    clients = [ClientEnemyTwo, ClientAi, ClientSalar]
+
+    random.shuffle(clients)
+
+    return [client(kernel) for client in clients]
 
 
 def get_kernel():
@@ -70,34 +80,40 @@ def get_kernel():
     return Kernel(kernel_main_game, kernel_config)
 
 
+def get_score_by_client(d: dict):
+    return "\n".join([f"{k}:\t{v}" for k, v in d.items()])
+
+
 def main():
-    n_iterations = 1
+    n_iterations = 1000
     for player in range(1):
-        wins = [0, 0, 0]
-        by_score = [0, 0, 0]
-        by_strategic = [0, 0, 0]
+        wins_by_pos = defaultdict(lambda: [0, 0, 0])
+        wins = defaultdict(lambda: 0)
+        by_score = defaultdict(lambda: 0)
+        by_strategic = defaultdict(lambda : 0)
         losses_list = []
         for i in tqdm(range(n_iterations)):
             block_print()
             # Creating and running game
             kernel = get_kernel()
-            clients = get_clients(kernel, player)
+            clients = get_clients_random(kernel, player)
             wp, end_type = run_game(kernel, clients)
 
             enable_print()
-            wins[wp] += 1
+            wins[clients[wp].__name__()] += 1
+            wins_by_pos[clients[wp].__name__()][wp] += 1
             if end_type == 'by_score':
-                by_score[wp] += 1
+                by_score[clients[wp].__name__()] += 1
             else:
-                by_strategic[wp] += 1
+                by_strategic[clients[wp].__name__()] += 1
             if wp != player:
                 losses_list.append(i)
         print(f'player turn: {player}')
         print(f'lost games: {losses_list}')
-        print(f'player names: {", ".join([client.__name__() for client in clients])}')
-        print(f'wins: {wins}')
-        print(f'by_score: {by_score}')
-        print(f'by_strategic: {by_strategic}')
+        print(f'---wins---\n{get_score_by_client(wins)}')
+        print(f'---wins by pos---\n{get_score_by_client(wins_by_pos)}')
+        print(f'---by score---\n{get_score_by_client(by_score)}')
+        print(f'---by strategic---\n{get_score_by_client(by_strategic)}')
 
 
 def run_game(kernel, clients, game_vis_file_name=None):
